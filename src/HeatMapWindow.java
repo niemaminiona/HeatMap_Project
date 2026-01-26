4063029b87d9155fa34c30ee37cfd7b0151db85a
filip.wojcik <filip.wojcik@SL2-36>
2026-01-26 12:18:08 +0100

    fixed heatmap, fixed function that returns counted neighbours

--- src/HeatMapWindow.java
+++ src/HeatMapWindow.java
@@ -32,8 +32,8 @@ class HeatMapGraphicPanel extends JPanel{
     public HeatMapGraphicPanel(int mapSize, int plotSize){
         this.mapSize = mapSize;
         this.plotSize = plotSize;
-        this.diversity = 10;
-        heatCellsList = returnRandomHeatMap(mapSize);
+        this.diversity = 8;
+        heatCellsList = returnCountedNeighbours(returnRandomHeatMap(mapSize));
 
         this.setPreferredSize(new Dimension(mapSize * plotSize, mapSize * plotSize));
     }
@@ -46,12 +46,21 @@ class HeatMapGraphicPanel extends JPanel{
         for (int y = 0; y < heatCellsList.length; y++) {
             for (int x = 0; x < heatCellsList.length; x++) {
 
-                int value = heatCellsList[y][x].value;
-                int c = 255 - value * 255 / (diversity - 1);
+                int value = heatCellsList[y][x].numberOfNeighbours;
 
-                g2.setColor(new Color(255, c, c));
+                // clamp just in case
+                value = Math.min(value, 8);
+
+                float ratio = value / 8.0f;
+
+                // higher neighbour count → more red
+                int red   = 255;
+                int green = (int)(255 * (1 - ratio));
+                int blue  = (int)(255 * (1 - ratio));
+
+                g2.setColor(new Color(red, green, blue));
+
                 g2.fillRect(y * plotSize, x * plotSize, plotSize, plotSize);
-
                 g2.setColor(Color.black);
                 g2.drawRect(y * plotSize, x * plotSize, plotSize, plotSize);
             }
@@ -71,35 +80,35 @@ class HeatMapGraphicPanel extends JPanel{
         return heatMap;
     }
 
-    private HeatCell[][] returnCountedNeighbours(HeatCell[][] heatMap){
-        HeatCell[][] updatedHeatMap = new HeatCell[heatMap.length][heatMap.length];
+    private HeatCell[][] returnCountedNeighbours(HeatCell[][] heatMap) {
+        int size = heatMap.length;
+        HeatCell[][] updatedHeatMap = new HeatCell[size][size];
 
-
-        final Point[] listOfNeighboursIndexes = {
+        final Point[] neighbours = {
                 new Point(-1, -1), new Point(0, -1), new Point(1, -1),
-                new Point(-1,  0), new Point(0,  0), new Point(1,  0),
+                new Point(-1,  0),                         new Point(1,  0),
                 new Point(-1,  1), new Point(0,  1), new Point(1,  1)
         };
 
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
 
+                int neighbourCount = 0;
 
+                for (Point p : neighbours) {
+                    int ny = y + p.y;
+                    int nx = x + p.x;
 
-        for(int y = 0; y < heatMap.length; y++){
-            for(int x = 0; x < heatMap.length; x++){
-                for(int count = 0; count < listOfNeighboursIndexes.length; count++){
-                    Point currentIndexPointed = listOfNeighboursIndexes[count];
-                    int indexedY = y + currentIndexPointed.y;
-                    int indexedX = x + currentIndexPointed.x;
-                    HeatCell selectedHeaCell = heatMap[indexedY][indexedX];
-                    if(currentIndexPointed.equals(new Point(0, 0)) && indexedY < 0 && x + indexedX < 0){
-                        continue;
-                    }else {
-                        if()
+                    // bounds check
+                    if (ny >= 0 && ny < size && nx >= 0 && nx < size && heatMap[ny][nx].value > 4) {
+                        neighbourCount++;
                     }
+
                 }
+
+                updatedHeatMap[y][x] = new HeatCell(heatMap[y][x].value, neighbourCount);
             }
         }
-
         return updatedHeatMap;
     }
 }
@@ -107,9 +116,15 @@ class HeatMapGraphicPanel extends JPanel{
 //class for data of single cell
 class HeatCell{
     public int value;
-    public int numberOfNeighbours = 0;
+    public int numberOfNeighbours;
 
     public HeatCell(int value){
         this.value = value;
+        this.numberOfNeighbours = 0;
+    }
+
+    public HeatCell(int value, int numberOfNeighbours){
+        this.value = value;
+        this.numberOfNeighbours = numberOfNeighbours;
     }
 }
